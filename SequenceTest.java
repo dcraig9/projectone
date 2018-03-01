1c504d03bc9662e1616e3ca18320d473a6c42c76
dcraig9 <36308131+dcraig9@users.noreply.github.com>
2018-03-01 10:57:55 -0500

    Add files via upload

--- SequenceTest.java
+++ SequenceTest.java
@@ -1,5 +1,5 @@
 /* SequenceTest.java
-*  Driver file to test DoubleSequenceArray class
+*  Driver file to test DoubleArraySeq class
 *
 *
 *
@@ -61,12 +61,16 @@ class SequenceTest {
       System.out.println();
       switch (menu)                       
       {
-         case 1: //print seqA and seqB
+         case 1: printSeq(sequenceA, 'A');
+                 printSeq(sequenceB, 'B');
                  break;
          case 2: System.out.println("SequenceA has a capacity of " + sequenceA.getCapacity() ); //print capacity of A & B
                  System.out.println("SequenceB has a capacity of " + sequenceB.getCapacity() );
                  break;
-         case 3: //report if A & B are equal
+         case 3: if (sequenceA.equals(sequenceB))
+                    System.out.println("SequenceA & SequenceB are equal.");
+                 else
+                    System.out.println("SequenceA & SequenceB are not equal.");//report if A & B are equal
                  break;
          case 4: if (active=='A')          //change active sequence
                  {
@@ -83,12 +87,12 @@ class SequenceTest {
                  if (active=='A')
                  {
                     sequenceA.addFront(newData);
-                    //print out updated sequence here
+                    printSeq(sequenceA, 'A');
                  }
                  else
                  {
                     sequenceB.addFront(newData);
-                    //print out updated sequence here
+                    printSeq(sequenceB, 'B');
                  }
                  break;
          case 6:
@@ -99,24 +103,35 @@ class SequenceTest {
                  if (active=='A')
                  {
                     sequenceA.addEnd(newData);
-                    //print out updated sequence here
+                    printSeq(sequenceA, 'A');
                  }
                  else
                  {
                     sequenceB.addEnd(newData);
-                    //print out updated sequence here
+                    printSeq(sequenceB, 'B');
                  }
                  break;
-         case 9:
+         case 9: sequenceA.addAll(sequenceB);
+                 printSeq(sequenceA, 'A');
                  break;
-         case 10:
+         case 10:delAtIndex(active,sequenceA,sequenceB);
                  break;
-         case 11:
+         case 11:if (active=='A')
+                 {
+                    sequenceA.removeFront();
+                    printSeq(sequenceA, 'A');
+                 }
+                 else
+                 {
+                    sequenceB.removeFront();
+                    printSeq(sequenceB, 'B');
+                 }
+
                  break;
-         case 12:
+         case 12:showAtIndex(active,sequenceA,sequenceB);
                  break;
-         case 13:
-                 break;
+         case 13: showLast(active,sequenceA,sequenceB);
+                  break;
          case 14: sequenceA.trimToSize();
                   sequenceB.trimToSize();
                   System.out.println("Excess memory trimmed from both sequences.");
@@ -137,19 +152,20 @@ class SequenceTest {
    
    public static void printMenu()
    {
-      System.out.println(" 1: Print the sequences of A and B");                     //
+      System.out.println();   
+      System.out.println(" 1: Print the sequences of A and B");                     //done
       System.out.println(" 2: Report the capacity of A and B");                     //done
-      System.out.println(" 3: Report if A and B are equal");                        //
+      System.out.println(" 3: Report if A and B are equal");                        //done
       System.out.println(" 4: Change active sequence (default is A)");              //done
-      System.out.println(" 5: Add a number to the front of a sequence");            //partial
+      System.out.println(" 5: Add a number to the front of a sequence");            //done
       System.out.println(" 6: Add a number before a given number(user inputted)");  //
       System.out.println(" 7: Add a number after a given number(user inputted)");   //
-      System.out.println(" 8: Add a number to the end of a sequence");              //partial
-      System.out.println(" 9: Add sequence B to end of A");                         //
-      System.out.println("10: Delete a number at a certain index");                 //
-      System.out.println("11: Delete the first number from the sequence");          //
-      System.out.println("12: Display a number at a certain index");                //
-      System.out.println("13: Display the last element in the sequence");           //
+      System.out.println(" 8: Add a number to the end of a sequence");              //done
+      System.out.println(" 9: Add sequence B to end of A");                         //done
+      System.out.println("10: Delete a number at a certain index");                 //done
+      System.out.println("11: Delete the first number from the sequence");          //partial, needs error handling
+      System.out.println("12: Display a number at a certain index");                //done
+      System.out.println("13: Display the last element in the sequence");           //done
       System.out.println("14: Trim extra memory from both A and B");                //done
       System.out.println("15: Create a clone sequence and show");                   //
       System.out.println("16: Create a new sequence using concatenate of B and A and show"); //
@@ -218,6 +234,77 @@ class SequenceTest {
    
    }
    
+   public static void printSeq(DoubleArraySeq seq, char active)
+   {
+      System.out.println();
+      System.out.println("Sequence" + active + " = " + seq );
+      System.out.println();
+   }
+   
+   public static void showLast(char active, DoubleArraySeq sequenceA, DoubleArraySeq sequenceB)
+   {
+      if (active=='A')
+      {
+         sequenceA.setCurrentLast();
+         System.out.println("Last element of SequenceA is " + sequenceA.getCurrent());
+      }
+      else
+      {
+         sequenceB.setCurrentLast();
+         System.out.println("Last element of SequenceB is " + sequenceB.getCurrent());
+      }
+
+   }
+   
+   public static void delAtIndex(char active, DoubleArraySeq seqA, DoubleArraySeq seqB)
+   {
+      int target;
+      System.out.println("Value at the index you choose will be deleted.");
+      target=getInt();
+      if (active=='A')
+         if (target > (seqA.size()-1))
+            System.out.println("SequenceA does not have that many elements. Operation canceled.");
+         else
+         {
+            seqA.setCurrent(target);
+            seqA.removeCurrent();
+            System.out.println("Element removed.");
+            printSeq(seqA,active);
+         }
+      else
+         if (target > (seqB.size()-1))
+            System.out.println("SequenceB does not have that many elements. Operation canceled.");
+         else
+         {
+            seqB.setCurrent(target);
+            seqB.removeCurrent();
+            System.out.println("Element removed.");
+            printSeq(seqB,active);
+         }
+   }
+   
+   public static void showAtIndex(char active, DoubleArraySeq seqA, DoubleArraySeq seqB)
+   {
+   int target;
+      System.out.println("Which index do you want to display?");
+      target=getInt();
+      if (active=='A')
+         if (target > (seqA.size()-1))
+            System.out.println("SequenceA does not have that many elements. Operation canceled.");
+         else
+         {
+            seqA.setCurrent(target);
+            System.out.println("Index #" + target + " of sequenceA is " + seqA.getCurrent() ); 
+         }
+      else
+         if (target > (seqB.size()-1))
+            System.out.println("SequenceB does not have that many elements. Operation canceled.");
+         else
+         {
+            seqB.setCurrent(target);
+            System.out.println("Index #" + target + " of sequenceA is " + seqA.getCurrent() );  
+         }
+   }
    
 } // end code
    
